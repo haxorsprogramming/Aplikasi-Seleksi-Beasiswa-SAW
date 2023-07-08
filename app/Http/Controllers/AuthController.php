bb0645af5ae476eddb25cb1c51dd3623d8089af2
Aditia Darma Nasution <ad.nasution_pro@alphaphilindo.com>
2023-07-08 22:47:27 +0800

    update add asset public

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function loginPage()
+    {
+        return view('auth.loginPage');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [HomeController::class, 'homePage']);
+Route::get('/auth/login', [AuthController::class, 'loginPage']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
